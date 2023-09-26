3d57e41e3bd588bda205da932c084de6990b972d
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2023-09-26 19:57:57 +0600

    Add files via upload

---
+++ riderlogin.php
@@ -0,0 +1,64 @@
+<?php 
+include 'db.php';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $email=$_POST['email'];
+    $password=md5($_POST['password']);
+    $sql="SELECT * FROM `rider` WHERE `email`='$email' AND `password`='$password'";
+    $result = mysqli_query($conn, $sql);
+	$data = $result->fetch_assoc() ;
+	$num = mysqli_num_rows($result);
+	if ($num == 1){
+        session_start();
+		$_SESSION['rid'] = $data['id'] ;
+        header("location: riderhome.php");
+
+
+    }
+
+
+}
+
+?>
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+
+    <title>Rider Login</title>
+  </head>
+  <body>
+    <center> <h1>Rider Login </h1>
+
+    
+    <div>
+
+
+    <form action="" method="post">
+
+
+         <label for="">Email</label><br>
+         <input type="text" placeholder="Email" name="email"><br>
+         <label for="">Password</label><br>
+         <input type="password" placeholder="password" name="password"><br><br>
+         <button type="submit">Login</button>
+
+
+
+    </form>
+    </div>
+    </center>
+    
+
+   
+  </body>
+</html>
