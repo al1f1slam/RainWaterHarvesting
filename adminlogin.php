6670f7147fe6b94660f5428a80ab315665cbc6b6
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2023-09-26 19:56:58 +0600

    Add files via upload

---
+++ adminlogin.php
@@ -0,0 +1,61 @@
+<?php 
+include 'db.php';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+$email =$_POST['email'];
+$password=md5($_POST['password']);
+$sql="SELECT * FROM `admin` WHERE `email`='$email' AND `password`='$password'";
+    $result = mysqli_query($conn, $sql);
+	$data = $result->fetch_assoc() ;
+	$num = mysqli_num_rows($result);
+	if ($num == 1){
+        session_start();
+		$_SESSION['aid'] = $data['id'] ;
+        header("location: adminhome.php");
+
+
+    }
+
+}
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+
+    <title>Admin Login</title>
+  </head>
+  <body>
+    <center> <h1>Admin Login </h1>
+
+    
+    <div>
+
+
+    <form action="" method="post">
+
+
+         <label for="">Email</label><br>
+         <input type="email" placeholder="User email" name="email"><br>
+         <label for="">Password</label><br>
+         <input type="password" placeholder="password" name="password"><br><br>
+         <button type="submit">Login</button>
+
+
+
+    </form>
+    </div>
+    </center>
+    
+
+   
+  </body>
+</html>
