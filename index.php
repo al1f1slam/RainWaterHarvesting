e7b6c189ec6ba5172e9aa5ce5d7b1864accec7d6
Alif Islam Siam <65067674+al1f1slam@users.noreply.github.com>
2023-09-26 19:58:39 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+
+    <title>Welcome </title>
+  </head>
+  <body>
+   
+
+  <table>
+<tr>
+
+<td><a href = 'login.php'><button>User Login</button></a></td>
+<td><a href = 'singup.php'><button>User Singup</button></a></td>
+
+<td><a href = 'riderapply.php'><button>Rider Apply</button></a></td>
+<td><a href = 'riderlogin.php'><button>Rider Login</button></a></td>
+</tr>
+
+</table>
+<center> <h1> Welcome to RAIN WATER HARVESTING</h1> </center>
+
+
+<h2>What Is Rain Water Harvesting?</h2>
+<p>Rain Water harvesting is a natural gift by almighty Allah. It is also called rain water collection technology or rainwater treatment system. It is a technology that collect the water at rainy season and store it for daily human use. Rainwater treatment system is epoch-useful decision for the Bangladesh as well as worldwide. Rainwater can be source of our daily water needed.By proper treatment of collected rain water can use for drinking purpose also. The rainwater those are drinkable, can use for different purpose. Such as irrigation like gardening. Like flush at toilet or car wash. It can also use for launder clothes. And it can also use our daily life. Such as taking shower, washing hand. In dry season it can be great asset as well. So, finally we can summarize Rain Water harvesting is a natural source. And it is collected storage at rainy season from roof tops, parks, road or from open grounds. And make treatment as require.</p>
+
+<img src="https://greendotbd.com/assets/uploads/media-uploader/large-rain-water-harvesting-730x410-11683371019.jpg"  alt=""/>
+<h3>Why Is RainwaterHarvesting Necessary?</h3>
+
+<p>In recently our climate are changing continuously. As a result, water crisis is one of our major concern. So, we have to think alternative source of water. Since weather becomes unpredictable, so it is the perfect time to think rainwater harvesting. By collecting rain water at rainy season we can use it at dry season. For the point of Environmental factors view, rainwater harvesting is also dependable for individuals to save money. It can capture freely from natural resource. As the environmental engineering companies continue to increase their prices due to population size. So, rainwater harvesting is essential to save our environment.</p>
+<p>Recently United Statement of America approve national standards in the U.S. Energy Policy Act of 1992. And it has been set minimum effectiveness standards for all toilets flushing, bath, gardening, urinals and faucets manufactured in the United States. The opportunities for inside water maintenance has decreased. At this time, use of outdoor water effectiveness standards have somewhat been neglected. Therefore, rainwater harvesting provides a tremendous opportunity to achieve more water conservation gains at each and every residential and commercial property.</p>
+<p>The below diagram shows the gains that can be indoor water fixtures through the combination of approximate standards and innovation by fixture establishments. As you can see, we don’t have much more room to go in terms of achieving more efficiency gains with our indoor fixtures. What’s next… the 0.2 gallon per flush toilet? Probably not! This phenomenon is recognized as the law of diminishing returns. So where will the next revolution in water conservation take</p>
+
+  </body>
+</html>
